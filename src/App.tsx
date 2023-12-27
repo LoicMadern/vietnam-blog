3d0ef5e3254f5768b79f872de94f8d8fda048ae1
LoicMadern <loicmaderns1@gmail.com>
2023-12-27 10:16:59 +0100

    enable routing and navigation close routage #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import ListPosts from "./components/ListPosts";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./ui/About";
+import NoMatch from "./ui/NoMatch";
+import Home from "./ui/Home";
 
 function App() {
   return (
-    <div className="App">
-      <ListPosts></ListPosts>
+    <div>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/ui/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>You are home</p>
+    </div>
+  );
+}
