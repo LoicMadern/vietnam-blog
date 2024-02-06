34d4957e9f0d7c4cdad9e390a3d80b2c5a605024
LoicMadern <loicmaderns1@gmail.com>
2024-02-06 20:20:32 +0100

    unused components and corresponding test modifications

--- src/components/__tests__/Post.test.tsx
+++ src/components/__tests__/Post.test.tsx
@@ -1,16 +1,8 @@
 import { cleanup, render, screen } from "@testing-library/react";
-import Post from "../post/Post";
-
-const mock_post = {
-  id: 1,
-  title: "Bird Valley",
-  description: "Natural bird reserve",
-  author: "Loic",
-  image: "valley",
-};
+import PostText from "../post/PostText";
 
 function generateComponent() {
-  render(<Post post={mock_post} />);
+  render(<PostText title={"titre"} />);
 }
 
 describe("initial component rendering", () => {
@@ -21,26 +13,8 @@ describe("initial component rendering", () => {
   afterEach(cleanup);
 
   test("title rendering", () => {
-    const title_expected = mock_post.title;
+    const title_expected = "titre";
     const linkElement = screen.getByText(title_expected);
     expect(linkElement).toBeInTheDocument();
-  });
-
-  test("description rendering", () => {
-    const description_expected = mock_post.description;
-    const linkElement = screen.getByText(description_expected);
-    expect(linkElement).toBeInTheDocument();
-  });
-
-  test("author rendering", () => {
-    const author_expected = mock_post.title;
-    const linkElement = screen.getByText(author_expected);
-    expect(linkElement).toBeInTheDocument();
-  });
-
-  test("image rendering", () => {
-    const endpoint_expected = mock_post.image + ".jpg";
-    const landscape = screen.getByRole("img");
-    expect(landscape).toHaveAttribute("src", endpoint_expected);
   });
 });
